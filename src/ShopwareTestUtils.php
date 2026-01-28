cc5ecc4271acaad9ba7e4d98e125dc153705719f
Raffaele Carelle <raffaele.carelle@algoritma.it>
2026-01-28 10:37:36 +0100

    Revert "Migrate `ShopwareTestUtils` from `Plugin` to `Bundle` and update `composer.json` to redefine project type as `library`. Remove Shopware-specific plugin configuration."
    This reverts commit 6afe74efce2e706d0637fab73dd4355b1e77e942.

--- src/ShopwareTestUtils.php
+++ src/ShopwareTestUtils.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Algoritma\ShopwareTestUtils;
 
 use Algoritma\ShopwareTestUtils\DependencyInjection\CompilerPass\FactoryRegistryCompilerPass;
-use Shopware\Core\Framework\Bundle;
+use Shopware\Core\Framework\Plugin;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class ShopwareTestUtils extends Bundle
+class ShopwareTestUtils extends Plugin
 {
     public function build(ContainerBuilder $container): void
     {
